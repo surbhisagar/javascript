83abe128d6c285956a7d5e64cfd6b80b2434e857
surbhisagar <132764106+surbhisagar@users.noreply.github.com>
2025-02-15 09:03:52 +0000

    datatypes and variable

---
+++ 1_first/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+
+
+//alert(3+3)  we are using node.js, not browser
+console.log(3+3)
+console.log("surbhi")
+
+
+let age=19
+let name="surbhi"
+let isLoggedIn=false
+let state;
+//PRIMETIVE DATATYPES
+//number
+//string=""
+//boolean= true/false
+//null=standalone value it's a object
+//undefined=
+//symbol= uniquE
+
+//OBJECT
+console.log(typeof undefined)//undefined
+console.log(typeof null) //object
---
+++ 1_first/variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="surbhisagar2508@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState
+// accountId=2 not allowed
+
+accountEmail="sur@gmail.com"
+accountPassword="2355"
+accountCity="ncr"
+
+//prefer not use var becuse of issue in block scope and functional scope
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
