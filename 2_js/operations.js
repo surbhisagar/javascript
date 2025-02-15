e5c716e919d42bd29da861acbacae99836cfa3e4
surbhisagar <132764106+surbhisagar@users.noreply.github.com>
2025-02-15 10:56:09 +0000

    conversion and operations

---
+++ 2_js/operations.js
@@ -0,0 +1,26 @@
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+//BASIC OPERATION
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="surbhi"
+let str2=" avni"
+let str3=str1+str2
+//console.log(str3)
+
+//console.log("1" + 2 + 2)
+//console.log(1 +2 +"2")
+//console.log((3+4)*5%3)this is the right idea for the operation
+
+// console.log(+true)
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
